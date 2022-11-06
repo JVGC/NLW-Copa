02fdbafb8e3b1cc67cd2f84375625287545967d3
JVGC <joaovictorgarciacoelho@gmail.com>
2022-11-06 18:50:09 -0300

    Adding other routes to the backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,10 @@ import fastify from "fastify"
 import { PrismaClient } from "@prisma/client"
 import fastifyCors from "@fastify/cors"
 
+import { z } from 'zod'
+
+import ShortUniqueId from 'short-unique-id'
+
 
 const prisma =  new PrismaClient({
     log: ['query']
@@ -20,7 +24,40 @@ async function bootstrap(){
         return { count }
     })
 
+    fastifyServer.get('/api/users/count', async () => {
+        const count = await prisma.user.count()
+        return { count }
+    })
+
+    fastifyServer.get('/api/guesses/count', async () => {
+        const count = await prisma.guess.count()
+        return { count }
+    })
+
+    fastifyServer.post('/api/pools', async (request, response) => {
+
+        const createPoolBody = z.object({
+            title: z.string()
+        })
+
+        const { title } = createPoolBody.parse(request.body)
+
+        const generate = new ShortUniqueId({ length: 6 })
+        const code = String(generate()).toUpperCase()
+
+        await prisma.pool.create({
+            data:{
+                title,
+                code
+            }
+        })
+
+        return response.status(201).send({ title, code })
+    })
+
     await fastifyServer.listen({ port: 3000, host: '0.0.0.0' })
 }
+
+
 
 bootstrap()
